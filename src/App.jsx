9d822005c8e347b7b5a6db819cc374f8eb28d4a4
raihana <taraihana@gmail.com>
2025-07-18 13:50:17 +0530

    header created progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0) 
 
   return (
     <>
-     
+     hhjjjjjj
+     <BrowserRouter>
+     <Routes>
+      <Route path='/header' element={<Header/>}/>
+     </Routes>
+     </BrowserRouter> 
+
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Header = () => {
+
   return (
-    <div>Header</div>
+    <div>hello
+        gkjgfy
+    </div>
   )
 }
 
