86f6536dfab2aa4a5cfa653e7d1fb776af982909
raihana <taraihana@gmail.com>
2025-07-30 14:58:47 +0530

    eventliste in progress

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -2,47 +2,51 @@ import React from 'react'
 
 const EventList = () => {
     return (
-        <section style={{minHeight:'1000px'}}>
+        <section style={{ minHeight: '1000px' }}>
             <div className="container">
                 <div className='eventListGrid'>
-                     
-                <div className='Cards-event-list'>
-                  <div className='gridimgDiv'><img src="/public/images/eventlistImg1.png" alt="" /></div>
-                   <h4>Startup Domain Summit-2025</h4>
-                   <span><i></i></span><label htmlFor="">Fri, Aug 8 2025 at 10:00 AM - 2:00 PM IST</label>
-                     <span><i></i></span><label htmlFor=""> 10:00AM -  2:00PM(IST) </label>
-                      <span><i></i></span><label htmlFor="">Golden Jubilee Hall, Bengaluru</label>
-                      <div>
-                        <div><span><i></i></span><p>₹ 299</p></div>
-                          <div><span><i></i></span><p>Book Now</p></div>
-                      </div>
-                </div>
-                <div className='Cards-event-list'>
-                    
-                    
-                </div>
-                <div className='Cards-event-list'>
-                  
-                </div>
-                <div className='Cards-event-list'>
-                  
-                </div>
-                <div className='Cards-event-list'>
-                   
-                </div>
-                <div className='Cards-event-list'>
-                   
-                </div>
-                <div className='Cards-event-list'>
-                    
-                </div>
-                <div className='Cards-event-list'>
-                   
-                </div>
-                <div className='Cards-event-list'>
-                   
-                </div>
-                
+
+                    <div className='Cards-event-list'>
+                        <div className='gridimgDiv'>
+                            <img src="/public/images/eventlistImg1.png" alt="" />
+                        </div>
+                        <div style={{ paddingLeft: '15px' }}>
+                            <h4>Startup Domain Summit-2025</h4>
+                            <span><i></i></span><label htmlFor="">Fri, Aug 8 2025 at 10:00 AM - 2:00 PM IST</label>
+                            <span><i></i></span><label htmlFor=""> 10:00AM -  2:00PM(IST) </label>
+                            <span><i></i></span><label htmlFor="">Golden Jubilee Hall, Bengaluru</label>
+                            <div>
+                                <div><span><i></i></span><p>₹ 299</p></div>
+                                <div><span><i></i></span><p>Book Now</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='Cards-event-list'>
+
+
+                    </div>
+                    <div className='Cards-event-list'>
+
+                    </div>
+                    <div className='Cards-event-list'>
+
+                    </div>
+                    <div className='Cards-event-list'>
+
+                    </div>
+                    <div className='Cards-event-list'>
+
+                    </div>
+                    <div className='Cards-event-list'>
+
+                    </div>
+                    <div className='Cards-event-list'>
+
+                    </div>
+                    <div className='Cards-event-list'>
+
+                    </div>
+
                 </div>
             </div>
         </section>
